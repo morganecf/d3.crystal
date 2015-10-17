57af567865f41d7ba213c18f5e3192033321ca50
Morgane Ciot <morganeciot@gmail.com>
2015-10-17 17:05:09 -0400

    first comet

---
+++ d3.crystal.js
@@ -0,0 +1,28 @@
+// (function() {
+//   d3.layout.force.prototype.crystal = function () {
+//     //return this.attr('data-editable', true);
+//     console.log("crystallizing");
+//   };
+// })();
+
+d3.crystal = function () {
+	var label = '';
+
+	//function 
+};
+
+// var force = d3.layout.force()
+//     .nodes(nodes)
+//     .links(links)
+//     .size([w, h])
+//     .linkStrength(0.1)
+//     .friction(0.9)
+//     .linkDistance(20)
+//     .charge(-30)
+//     .gravity(0.1)
+//     .theta(0.8)
+//     .alpha(0.1)
+//     .start();
+
+// https://github.com/mbostock/d3/wiki/Force-Layout
+// how to get it all wiggly again after it crystallizes? force.resume? 
